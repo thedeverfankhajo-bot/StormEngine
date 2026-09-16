2b2e7ff51953e97ef595cffaed078643ed58d9ae
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 20:56:14 +0330

    Test Mesh vertex layouts

--- tests/mesh_tests.cpp
+++ tests/mesh_tests.cpp
@@ -12,18 +12,32 @@ int main() {
     const BufferHandle vertex(1);
     const BufferHandle index(2);
 
-    Mesh nonIndexed(MeshDesc{vertex, {}, 3, 0, IndexType::UInt16, PrimitiveTopology::Triangles});
+    VertexLayout layout{};
+    layout.attributeCount = 2;
+    layout.stride = 28;
+    layout.attributes[0] = {0, VertexFormat::Float32x3, 0};
+    layout.attributes[1] = {1, VertexFormat::Float32x4, 12};
+    assert(layout.valid());
+
+    Mesh nonIndexed(MeshDesc{vertex, {}, 3, 0, IndexType::UInt16, PrimitiveTopology::Triangles, layout});
     assert(nonIndexed.valid());
     assert(!nonIndexed.indexed());
     assert(nonIndexed.drawCommand().vertexCount == 3);
     assert(!nonIndexed.drawCommand().indexed());
+    assert(nonIndexed.drawCommand().vertexLayout.stride == 28);
+    assert(nonIndexed.drawCommand().vertexLayout.attributeCount == 2);
+    assert(nonIndexed.drawCommand().vertexLayout.attributes[1].offset == 12);
 
-    Mesh indexed(MeshDesc{vertex, index, 8, 6, IndexType::UInt16, PrimitiveTopology::Triangles});
+    Mesh indexed(MeshDesc{vertex, index, 8, 6, IndexType::UInt16, PrimitiveTopology::Triangles, layout});
     assert(indexed.valid());
     assert(indexed.indexed());
     assert(indexed.drawCommand().indexed());
+    assert(indexed.drawCommand().vertexLayout.attributes[0].format == VertexFormat::Float32x3);
 
-    Mesh broken(MeshDesc{vertex, {}, 8, 6, IndexType::UInt16, PrimitiveTopology::Triangles});
+    Mesh noLayout(MeshDesc{vertex, {}, 3, 0, IndexType::UInt16, PrimitiveTopology::Triangles, {}});
+    assert(!noLayout.valid());
+
+    Mesh broken(MeshDesc{vertex, {}, 8, 6, IndexType::UInt16, PrimitiveTopology::Triangles, layout});
     assert(!broken.valid());
     assert(broken.indexed());
 
