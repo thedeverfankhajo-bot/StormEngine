0e0c849b7c439e12f8e6adaf9b22518c465a756e
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 19:46:53 +0330

    Add headless EGL GLES context abstraction

---
+++ engine/render/include/storm/render/GlesContext.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace storm::render {
+
+class GlesContext final {
+public:
+    GlesContext() = default;
+    ~GlesContext();
+
+    GlesContext(const GlesContext&) = delete;
+    GlesContext& operator=(const GlesContext&) = delete;
+
+    bool initializePbuffer(int width = 1, int height = 1) noexcept;
+    void shutdown() noexcept;
+    bool makeCurrent() noexcept;
+    bool swap() noexcept;
+
+    [[nodiscard]] bool valid() const noexcept { return valid_; }
+
+private:
+    bool valid_{false};
+    void* display_{nullptr};
+    void* surface_{nullptr};
+    void* context_{nullptr};
+};
+
+} // namespace storm::render
