76f395fd1a1f96e0d4cd4764f38fd80fa104f054
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 20:59:35 +0330

    Test shader resource lifecycle

--- tests/render_device_tests.cpp
+++ tests/render_device_tests.cpp
@@ -8,24 +8,30 @@ int main() {
     NullRenderDevice device;
     assert(device.liveBufferCount() == 0);
     assert(device.liveTextureCount() == 0);
+    assert(device.liveShaderCount() == 0);
     assert(device.submittedDrawCount() == 0);
 
     assert(!device.createBuffer(BufferDesc{}).valid());
     assert(!device.createTexture(TextureDesc{0, 64, 1}).valid());
     assert(!device.createTexture(TextureDesc{64, 64, 0}).valid());
+    assert(!device.createShader(ShaderDesc{ShaderStage::Vertex}, "").valid());
 
     const auto vertexBuffer = device.createBuffer(BufferDesc{1024, BufferUsage::Static});
     const auto indexBuffer = device.createBuffer(BufferDesc{512, BufferUsage::Static});
     const auto texture = device.createTexture(TextureDesc{64, 64, 1});
+    const auto shader = device.createShader(ShaderDesc{ShaderStage::Vertex}, "void main() {}");
     assert(vertexBuffer.valid());
     assert(indexBuffer.valid());
     assert(texture.valid());
+    assert(shader.valid());
     assert(device.liveBufferCount() == 2);
     assert(device.liveTextureCount() == 1);
+    assert(device.liveShaderCount() == 1);
 
     DrawCommand draw{};
     draw.vertexBuffer = vertexBuffer;
     draw.vertexCount = 3;
+    draw.shader = shader;
 
     assert(!device.submit(draw));
 
@@ -52,6 +58,11 @@ int main() {
     device.destroyBuffer(vertexBuffer);
     assert(device.liveBufferCount() == 1);
     assert(!device.submit(deadVertex));
+
+    DrawCommand deadShader = indexed;
+    device.destroyShader(shader);
+    assert(device.liveShaderCount() == 0);
+    assert(!device.submit(deadShader));
 
     device.endFrame();
     assert(device.submittedDrawCount() == 2);
