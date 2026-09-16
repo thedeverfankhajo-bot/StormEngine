8d9560d1bdfb5b4a91431989835edf409960676a
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 18:48:33 +0330

    Add ECS System interface

---
+++ engine/ecs/include/storm/ecs/System.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string_view>
+
+namespace storm::ecs {
+
+class Registry;
+
+class SystemContext {
+public:
+    explicit SystemContext(Registry& registry) noexcept : registry_(registry) {}
+
+    Registry& registry() noexcept { return registry_; }
+    const Registry& registry() const noexcept { return registry_; }
+
+private:
+    Registry& registry_;
+};
+
+class System {
+public:
+    virtual ~System() = default;
+
+    virtual std::string_view name() const noexcept = 0;
+    virtual void update(SystemContext& context, float deltaTime) = 0;
+};
+
+} // namespace storm::ecs
