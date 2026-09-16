62bc282cff367eac396068dcc3a8afb278d86ac7
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 19:05:49 +0330

    Expand ECS regression coverage

--- tests/ecs_tests.cpp
+++ tests/ecs_tests.cpp
@@ -8,32 +8,17 @@
 
 namespace {
 
-struct Position {
-    int x{};
-    int y{};
-};
-
-struct Velocity {
-    int x{};
-    int y{};
-};
-
-struct Tag {
-    int value{};
-};
+struct Position { int x{}; int y{}; };
+struct Velocity { int x{}; int y{}; };
+struct Tag { int value{}; };
 
 void testSparseSet() {
     storm::ecs::SparseSet set;
-    set.insert(10);
-    set.insert(20);
-    set.insert(30);
-
+    set.insert(10); set.insert(20); set.insert(30);
     assert(set.size() == 3);
     assert(set.contains(10));
     assert(set.position(20) == 1);
-
     set.erase(20);
-
     assert(!set.contains(20));
     assert(set.size() == 2);
     assert(set.contains(30));
@@ -42,12 +27,8 @@ void testSparseSet() {
 
 void testComponentStorageSwapBack() {
     storm::ecs::ComponentStorage<Position> storage;
-    storage.emplace(1, 10, 20);
-    storage.emplace(2, 30, 40);
-    storage.emplace(3, 50, 60);
-
+    storage.emplace(1, 10, 20); storage.emplace(2, 30, 40); storage.emplace(3, 50, 60);
     storage.remove(2);
-
     assert(storage.size() == 2);
     assert(storage.tryGet(2) == nullptr);
     assert(storage.tryGet(3) != nullptr);
@@ -59,10 +40,8 @@ void testRegistryGeneration() {
     storm::ecs::Registry registry;
     const auto first = registry.create();
     assert(registry.valid(first));
-
     registry.destroy(first);
     assert(!registry.valid(first));
-
     const auto reused = registry.create();
     assert(reused.id() == first.id());
     assert(reused.generation() != first.generation());
@@ -73,18 +52,14 @@ void testRegistryGeneration() {
 void testRegistryComponents() {
     storm::ecs::Registry registry;
     const auto entity = registry.create();
-
     registry.emplace<Position>(entity, 1, 2);
     registry.emplace<Velocity>(entity, 3, 4);
-
     assert(registry.has<Position>(entity));
     assert(registry.has<Velocity>(entity));
     assert(registry.get<Position>(entity).x == 1);
     assert(registry.get<Velocity>(entity).y == 4);
-
     registry.remove<Velocity>(entity);
     assert(!registry.has<Velocity>(entity));
-
     registry.destroy(entity);
     assert(!registry.has<Position>(entity));
 }
@@ -94,26 +69,33 @@ void testQueryUsesMatchingEntities() {
     const auto a = registry.create();
     const auto b = registry.create();
     const auto c = registry.create();
-
-    registry.emplace<Position>(a, 1, 0);
-    registry.emplace<Velocity>(a, 10, 0);
-
+    registry.emplace<Position>(a, 1, 0); registry.emplace<Velocity>(a, 10, 0);
     registry.emplace<Position>(b, 2, 0);
-
-    registry.emplace<Position>(c, 3, 0);
-    registry.emplace<Velocity>(c, 30, 0);
-    registry.emplace<Tag>(c, 99);
-
+    registry.emplace<Position>(c, 3, 0); registry.emplace<Velocity>(c, 30, 0); registry.emplace<Tag>(c, 99);
     std::vector<storm::ecs::Entity::Id> visited;
     registry.each<Position, Velocity>([&](auto entity, Position& position, Velocity& velocity) {
-        visited.push_back(entity.id());
-        position.x += velocity.x;
+        visited.push_back(entity.id()); position.x += velocity.x;
     });
-
     assert(visited.size() == 2);
     assert(registry.get<Position>(a).x == 11);
     assert(registry.get<Position>(c).x == 33);
     assert(registry.get<Position>(b).x == 2);
+}
+
+void testQueryWhenFirstComponentIsNotSmallest() {
+    storm::ecs::Registry registry;
+    const auto a = registry.create();
+    const auto b = registry.create();
+    const auto c = registry.create();
+    registry.emplace<Position>(a, 1, 0); registry.emplace<Velocity>(a, 10, 0);
+    registry.emplace<Position>(b, 2, 0); registry.emplace<Velocity>(b, 20, 0);
+    registry.emplace<Position>(c, 3, 0); registry.emplace<Velocity>(c, 30, 0);
+    registry.remove<Position>(c);
+    std::vector<storm::ecs::Entity::Id> visited;
+    registry.each<Position, Velocity>([&](auto entity, Position&, Velocity&) { visited.push_back(entity.id()); });
+    assert(visited.size() == 2);
+    assert(visited[0] != c.id());
+    assert(visited[1] != c.id());
 }
 
 } // namespace
@@ -124,5 +106,6 @@ int main() {
     testRegistryGeneration();
     testRegistryComponents();
     testQueryUsesMatchingEntities();
+    testQueryWhenFirstComponentIsNotSmallest();
     return 0;
 }
