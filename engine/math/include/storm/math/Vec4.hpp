385caf441324044465ac9eeae209df2611856c44
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 18:38:34 +0330

    math: add Vec4

---
+++ engine/math/include/storm/math/Vec4.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cmath>
+
+namespace storm::math {
+
+struct Vec4 {
+    float x{0.0f};
+    float y{0.0f};
+    float z{0.0f};
+    float w{0.0f};
+
+    constexpr Vec4() = default;
+    constexpr Vec4(float xValue, float yValue, float zValue, float wValue)
+        : x(xValue), y(yValue), z(zValue), w(wValue) {}
+
+    constexpr Vec4 operator+(const Vec4& rhs) const { return {x + rhs.x, y + rhs.y, z + rhs.z, w + rhs.w}; }
+    constexpr Vec4 operator-(const Vec4& rhs) const { return {x - rhs.x, y - rhs.y, z - rhs.z, w - rhs.w}; }
+    constexpr Vec4 operator*(float scalar) const { return {x * scalar, y * scalar, z * scalar, w * scalar}; }
+    constexpr Vec4 operator/(float scalar) const { return {x / scalar, y / scalar, z / scalar, w / scalar}; }
+
+    constexpr float lengthSquared() const { return x*x + y*y + z*z + w*w; }
+    float length() const { return std::sqrt(lengthSquared()); }
+    constexpr float dot(const Vec4& rhs) const { return x*rhs.x + y*rhs.y + z*rhs.z + w*rhs.w; }
+
+    Vec4 normalized() const {
+        const float len = length();
+        return len > 0.0f ? *this / len : Vec4{};
+    }
+};
+
+constexpr Vec4 operator*(float scalar, const Vec4& value) { return value * scalar; }
+
+} // namespace storm::math
