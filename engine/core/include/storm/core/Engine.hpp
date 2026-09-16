f4082fb75d91cd3fe88111fda82a98bb0f195ed0
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 18:53:19 +0330

    Expose GameLoop through Engine

--- engine/core/include/storm/core/Engine.hpp
+++ engine/core/include/storm/core/Engine.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "GameLoop.hpp"
 #include <string_view>
 
 namespace storm {
@@ -11,6 +12,12 @@ public:
 
     [[nodiscard]] std::string_view name() const noexcept;
     [[nodiscard]] std::string_view version() const noexcept;
+
+    GameLoop& gameLoop() noexcept { return gameLoop_; }
+    const GameLoop& gameLoop() const noexcept { return gameLoop_; }
+
+private:
+    GameLoop gameLoop_;
 };
 
 } // namespace storm
