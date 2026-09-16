7cd41ae830d3c562ad463627924f636c839d790f
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-16 19:04:58 +0330

    Fix Registry query iteration correctness

--- engine/ecs/include/storm/ecs/Registry.hpp
+++ engine/ecs/include/storm/ecs/Registry.hpp
@@ -21,7 +21,6 @@ public:
             alive_[id] = true;
             return Entity{id, generations_[id]};
         }
-
         const Entity::Id id = static_cast<Entity::Id>(alive_.size());
         alive_.push_back(true);
         generations_.push_back(0);
@@ -96,7 +95,7 @@ public:
     template <typename... Components, typename Func>
     void each(Func&& func) {
         static_assert(sizeof...(Components) > 0, "Registry::each requires at least one component");
-        eachSmallest<Components...>(std::forward<Func>(func));
+        eachImpl<Components...>(std::forward<Func>(func));
     }
 
     template <typename T>
@@ -114,11 +113,8 @@ private:
     template <typename T>
     struct Storage final : IStorage {
         ComponentStorage<T> data;
-
         template <typename... Args>
-        T& emplace(Entity::Id id, Args&&... args) {
-            return data.emplace(id, std::forward<Args>(args)...);
-        }
+        T& emplace(Entity::Id id, Args&&... args) { return data.emplace(id, std::forward<Args>(args)...); }
         bool has(Entity::Id id) const noexcept { return data.has(id); }
         T* tryGet(Entity::Id id) noexcept { return data.tryGet(id); }
         const T* tryGet(Entity::Id id) const noexcept { return data.tryGet(id); }
@@ -140,42 +136,16 @@ private:
         return *static_cast<Storage<T>*>(it->second.get());
     }
 
-    template <typename T>
-    const Storage<T>* findStorage() const noexcept {
-        const auto it = storages_.find(componentType<T>());
-        return it == storages_.end() ? nullptr : static_cast<const Storage<T>*>(it->second.get());
-    }
-
-    template <typename Candidate, typename... Others>
-    bool isSmallest() const {
-        const std::size_t count = componentCount<Candidate>();
-        return ((count <= componentCount<Others>()) && ...);
-    }
-
-    template <typename Candidate, typename... Components, typename Func>
-    bool tryQueryCandidate(Func&& func) {
-        if (!isSmallest<Candidate, Components...>()) return false;
-
-        const Storage<Candidate>* storage = findStorage<Candidate>();
-        if (!storage || storage->size() == 0) return true;
-
+    template <typename First, typename... Rest, typename Func>
+    void eachImpl(Func&& func) {
+        const Storage<First>* storage = findStorage<First>();
+        if (!storage) return;
         for (std::size_t i = 0; i < storage->size(); ++i) {
             const Entity::Id id = storage->entityAt(i);
             if (id >= alive_.size() || !alive_[id]) continue;
-
             Entity entity{id, generations_[id]};
-            if ((has<Components>(entity) && ...))
-                std::invoke(std::forward<Func>(func), entity, get<Candidate>(entity), get<Components>(entity)...);
-        }
-        return true;
-    }
-
-    template <typename First, typename... Rest, typename Func>
-    void eachSmallest(Func&& func) {
-        if constexpr (sizeof...(Rest) == 0) {
-            (void)tryQueryCandidate<First>(std::forward<Func>(func));
-        } else if (!tryQueryCandidate<First, Rest...>(func)) {
-            eachSmallest<Rest...>(std::forward<Func>(func));
+            if ((has<Rest>(entity) && ...))
+                std::invoke(std::forward<Func>(func), entity, get<First>(entity), get<Rest>(entity)...);
         }
     }
 
@@ -184,6 +154,12 @@ private:
             (void)type;
             storage->remove(id);
         }
+    }
+
+    template <typename T>
+    const Storage<T>* findStorage() const noexcept {
+        const auto it = storages_.find(componentType<T>());
+        return it == storages_.end() ? nullptr : static_cast<const Storage<T>*>(it->second.get());
     }
 
     std::vector<bool> alive_;
