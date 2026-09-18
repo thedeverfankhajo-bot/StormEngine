ab06e3320ddfe258a62eeef053bab24cdea6fe69
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-18 14:30:09 +0330

    Tests: cover cached scissor state

--- tests/render_state_cache_tests.cpp
+++ tests/render_state_cache_tests.cpp
@@ -10,9 +10,16 @@ int main() {
     assert(!cache.needsApply(state));
     auto changed = state;
     changed.blendEnabled = true;
+    changed.scissorEnabled = true;
+    changed.scissorX = -4;
+    changed.scissorY = 8;
+    changed.scissorWidth = 320;
+    changed.scissorHeight = 180;
     assert(cache.needsApply(changed));
     cache.markApplied(changed);
     assert(!cache.needsApply(changed));
+    assert(cache.state().scissorX == -4);
+    assert(cache.state().scissorWidth == 320);
     cache.invalidate();
     assert(!cache.valid());
     assert(cache.needsApply(changed));
