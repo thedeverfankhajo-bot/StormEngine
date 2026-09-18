ca2ef7c085a456982387ad80e4c0116920c5ad9a
TheDevErfan <thedeverfankhajo@gmail.com>
2026-09-18 12:07:46 +0330

    Upload matrix uniforms to GLES shaders

--- engine/render/src/GlesRenderDevice.cpp
+++ engine/render/src/GlesRenderDevice.cpp
@@ -71,6 +71,8 @@ bool applyMaterial(const Material& material,
             glUniform3fv(location, 1, vec3->data());
         } else if (const auto* vec4 = std::get_if<MaterialVec4>(&value)) {
             glUniform4fv(location, 1, vec4->data());
+        } else if (const auto* mat4 = std::get_if<MaterialMat4>(&value)) {
+            glUniformMatrix4fv(location, 1, GL_FALSE, mat4->data());
         } else if (const auto* texture = std::get_if<TextureHandle>(&value)) {
             if (!texture->valid() || textures.find(texture->id()) == textures.end() || textureUnit >= maxUnits) {
                 return false;
